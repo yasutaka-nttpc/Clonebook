cc77961ef7b109fec347d21fc5f9c873c7beec71
yasutaka <yasutaka@yasutaka-no-MacBook-Air.local>
2017-12-20 23:54:11 +0900

    投サインアップ時エラーメッセージ重複修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable
          
   has_many :topics, dependent: :destroy
   has_many :comments, dependent: :destroy
